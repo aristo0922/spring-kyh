d9cd1958222f006756a9d494b52c9d898bd56474
aristo <dkfud2121@naver.com>
2024-09-03 17:35:01 +0900

    class 20 spring database access skill

--- hello-spring/src/main/java/hello/hello_spring/Springconfig.java
+++ hello-spring/src/main/java/hello/hello_spring/Springconfig.java
@@ -17,13 +17,4 @@ public class Springconfig {
   public MemberRepository memberRepository(){
     return new MemoryMemeberRepository();
   }
-  /**
-   * 서비스와 레포 빈에 등록
-   * 등록된 레포를 서비스에 삽입
-   * 서비스-레포지토리 연결
-   *
-   * 컨트롤러는 스프링이 관리하기 때문에 @Controller 를 사용해야 한다.
-   * -> Autowired 로 서비스와 연결
-   */
-
 }
--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -15,5 +20,27 @@ public class MemberController {
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members/new")
+  public String creatForm(){
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm form){
+    Member member = new Member();
+    member.setName(form.getName());
+
+    memberService.join(member);
+
+    return "redirect:/" ; // 홈 화면으로
+  }
+
+  @GetMapping("/members")
+  public String list(Model model){
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
